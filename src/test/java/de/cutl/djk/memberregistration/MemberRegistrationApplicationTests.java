2c2475496ad0dc4a4cca79d6592d6d52173a5b7b
lange <lange@synyx.de>
2023-12-19 20:08:00 +0100

    initial commit

---
+++ src/test/java/de/cutl/djk/memberregistration/MemberRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cutl.djk.memberregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
